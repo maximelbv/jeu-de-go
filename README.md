74c2a50f730cd48997915e31c03352131eb4dafd
BManta <novoarodriguezmiguel@gmail.com>
2024-05-15 14:39:35 +0200

    update database

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # jeu-de-go
+pip install django==4.1
 
 pip install pytest pytest-django
 
@@ -7,5 +8,9 @@ pip install beautifulsoup4
 pip install html5lib
 
 pip install requests
+
+pip install mysqlclient
+
+python manage.py migrate
 
 pytest
--- plateforme_jeu/api/models.py
+++ plateforme_jeu/api/models.py
@@ -1,6 +1,33 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    # AbstractUser contient déjà les champs nom, mot de passe et email.
-    pass
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_admin=False):
+        if not email:
+            raise ValueError("L'email est requis")
+        user = self.model(email=self.normalize_email(email), is_admin=is_admin)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password=password, is_admin=True)
+        return user
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
