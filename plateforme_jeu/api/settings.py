91ff534c3c84cffe9c74ac8a7d4fd6473711ac87
BManta <novoarodriguezmiguel@gmail.com>
2024-06-12 14:17:54 +0200

    Add scraping and migrations

--- plateforme_jeu/api/settings.py
+++ plateforme_jeu/api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +81,8 @@ DATABASES = {
         'NAME': 'game_go',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': 'localhost', 
-        'PORT': '3306',     
+        'HOST': 'localhost',  
+        'PORT': '3306'
     }
 }
 
