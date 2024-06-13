ed3bcfc15b9678b63b494ade8038498565b0ca72
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-06-13 11:03:52 +0200

    fix not deleted files

--- api/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
-class UserRegisterForm(UserCreationForm):
-    pass
-
-class UserAuthenticationForm(AuthenticationForm):
-    pass
